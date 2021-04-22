63472787456b9da08d62dd25f85a47700a6309f4
mathgoncalves <matheusgoncalvesgraca@outlook.com>
2021-04-22 17:37:06 -0300

    Add publication model for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Publication (models.Model):
+    title = models.CharField(max_length=200)
+    preview_description = models.TextField()
+    description = models.TextField()
+    external_articles = models.TextField()
+    image = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#Create your models here.
